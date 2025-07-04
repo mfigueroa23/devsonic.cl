3db53e5fe0664562662ca9014ce81e62c7316091
 Marco Figueroa <marcofigueroa23@icloud.com>
2025-07-04 15:55:22 -0400

    feat: adding home component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { inject as VercelWebAnalytics } from '@vercel/analytics'
 import { injectSpeedInsights as VercelSpeedInsights } from '@vercel/speed-insights';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
