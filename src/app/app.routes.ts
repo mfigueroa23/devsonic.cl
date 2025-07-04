9a1f518e561bdc53182e0b3bf4b049ced74503f1
 Marco Figueroa <marcofigueroa23@icloud.com>
2025-07-04 16:52:56 -0400

    feat: adding components, routes and routerlink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('@components/home/home').then(comp => comp.Home) }
+  { path: '', loadComponent: () => import('@components/home/home').then(comp => comp.Home) },
+  { path: 'experiencia', loadComponent: () => import('@components/experiencia/experiencia').then(comp => comp.Experiencia) },
+  { path: 'proyectos', loadComponent: () => import('@components/proyectos/proyectos').then(comp => comp.Proyectos) },
+  { path: 'contacto', loadComponent: () => import('@components/contacto/contacto').then(comp => comp.Contacto) },
+  { path: '**', loadComponent: () => import('@components/not-found/not-found').then(comp => comp.NotFound) }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { inject as VercelWebAnalytics } from '@vercel/analytics'
 import { injectSpeedInsights as VercelSpeedInsights } from '@vercel/speed-insights';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
