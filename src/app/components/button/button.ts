947e6b5c2f58b6a17ba6d72179049e8716ffc1b5
Marco Antonio Figueroa Sanchez <marcofigueroa23@icloud.com>
2026-09-25 22:28:03 -0300

    feat(button): Adding disabled state
    This add the disabled input to the button

--- src/app/components/button/button.ts
+++ src/app/components/button/button.ts
@@ -2,7 +2,7 @@ import { NgTemplateOutlet } from '@angular/common';
 import { Component, computed, input } from '@angular/core';
 
 const base: string =
-  'relative overflow-hidden rounded-full font-md focus:outline-none focus-visible:ring-2 focus-visible:ring-primary bg-primary text-primary-foreground hover:bg-primary/90 shadow-lg shadow-primary/25 cursor-pointer';
+  'relative overflow-hidden rounded-full font-md focus:outline-none focus-visible:ring-2 focus-visible:ring-primary bg-primary text-primary-foreground hover:bg-primary/90 disabled:opacity-50 disabled:cursor-not-allowed shadow-lg shadow-primary/25 cursor-pointer';
 
 const sizes = {
   sm: 'px-4 py-2 text-sm',
@@ -22,6 +22,7 @@ export class Button {
   public readonly size = input<Size>('default');
   public readonly extraClass = input<string>('');
   public readonly href = input<string>('');
+  public readonly disabled = input<boolean>(false);
 
   protected readonly styles = computed(() =>
     `${base} ${sizes[this.size()]} ${this.extraClass()}`.trim(),
