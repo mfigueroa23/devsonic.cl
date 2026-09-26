0d290e2fc3e3e721065e39b9e0f267db57a4aeab
Marco Antonio Figueroa Sanchez <marcofigueroa23@icloud.com>
2026-09-25 23:31:12 -0300

    feat(ssr): Prerendering home page
    This add Angular SSR with static output so the home page HTML is generated at build time

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideClientHydration(),
+  ],
 };
